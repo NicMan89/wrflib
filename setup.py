2b06eec0d9ea2a0cf0fde48fa6cc0181840c9915
NicMan89 <nicolamanconi8@gmail.com>
2022-07-21 23:39:08 +0200

    Rename script --> v2.py
    Rinomina da v1.py a v2.py con modifica nel setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='v1',
+    name='v2',
     version='0.0.2',
     description='',
     url='https://github.com/NicMan89/wrflib',
