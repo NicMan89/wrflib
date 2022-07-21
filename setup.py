ef0d11d3b83611d7cedafd7cfc1621191f766b4f
NicMan89 <nicolamanconi8@gmail.com>
2022-07-21 19:35:30 +0200

    Rename WRFLIB.py
    Rinomina dello script e modifica in setup.py del nome file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='WRFLIB',
+    name='v1',
     version='0.0.2',
     description='',
     url='https://github.com/NicMan89/wrflib',
