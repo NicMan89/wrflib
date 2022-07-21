b960a5c2c2ed858b1d929c3913c5248baf7a17c2
NicMan89 <nicolamanconi8@gmail.com>
2022-07-21 18:24:39 +0200

    Remove Cartopy
    Rimozione cartopy da requirements.txt e dal setup.py, in quest'ultimo modifica del nome del file WRFLIB.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='v1',
+    name='WRFLIB',
     version='0.0.1',
     description='',
     url='https://github.com/NicMan89/wrflib',
@@ -13,7 +13,7 @@ setup(
     author_email='nicolamanconi8@gmail.com',
     license='MIT',
     install_requires=required,
-    packages=find_packages(include=['wrflib','wrflib.*']),
+    packages=find_packages(include=['WRFLIB']),
     zip_safe=False
 )
 #pipreqs /home/project/location
